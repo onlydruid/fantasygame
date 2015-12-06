eb8f6db97d63057fd7132b2ecad9332219735762
onlydruid <danny_hines@live.co.uk>
2015-12-06 01:26:23 +0000

    ElfenTrail Alpha v0.014c
    Fixed maintenance message

--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@ var goldOn = false;  /*variable for determining whether a gold coin is currently
 due to a bug only one gold coin could be present at a time but also to avoid users being able to overcrowd the page the bug was never fixed and became a feature*/
 
 
-if(window.innerHeight < window.innerWidth){
-    var img = document.createElement('img');
-	img.setAttribute("id", "landscapeblock");                      //attributes and style of created element
-	img.setAttribute("style", "position:absolute;");
-	img.setAttribute("src", "images/landscapeblock.jpg");
-	document.body.appendChild(img);
-	img.style.height = '100%';
-	img.style.width = '100%';
-	img.style.zIndex = "100";
-}
 
 
 //hiding elements at start of game
